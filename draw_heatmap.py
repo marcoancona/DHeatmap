ad8196088c3addd945f3fcd11333522afb6bc05f
Marco Ancona <marco.ancona@inf.ethz.ch>
2016-08-01 19:28:44 +0200

    Added RMSE estimation. New colors.

--- draw_heatmap.py
+++ draw_heatmap.py
@@ -13,8 +13,26 @@ from sample_distances import MAX_LAT, MAX_LON, MIN_LAT, MIN_LON
 MAX_X = 1000
 MAX_Y = 1000
 
-# at what distance should we stop making predictions?
-IGNORE_DIST = 0.01
+DRAW_DOTS = True
+
+colors = [
+    (255, 0, 0),
+    (255, 127, 0),
+    (255, 208, 0),
+    (255, 240, 0),
+    (255, 255, 0),
+    (218, 255, 0),
+    (176, 255, 0),
+    (128, 255, 0),
+    (0, 255, 0),
+    (0, 240, 255),
+    (0, 213, 255),
+    (0, 171, 255),
+    (0, 127, 255),
+]
+
+buckets = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+buckets.reverse()
 
 
 def ll_to_01(lat, lon):
@@ -37,48 +55,39 @@ def load_data(sample_file):
 
 def color(val, buckets):
     if val is None:
-        return 255, 255, 255, 0
-
-    colors = [(255, 0, 0),
-              (255, 91, 0),
-              (255, 127, 0),
-              (255, 171, 0),
-              (255, 208, 0),
-              (255, 240, 0),
-              (255, 255, 0),
-              (218, 255, 0),
-              (176, 255, 0),
-              (128, 255, 0),
-              (0, 255, 0),
-              (0, 255, 255),
-              (0, 240, 255),
-              (0, 213, 255),
-              (0, 171, 255),
-              (0, 127, 255),
-              (0, 86, 255),
-              (0, 0, 255),
-              ]
+        return 0, 0, 0, 0
 
     for threshold, color in zip(buckets, colors):
         if val > threshold:
             return color
-    return colors[-1]
+    return 0, 0, 0, 0
 
 
 def start(file_name):
     print "Loading data..."
     all_data = load_data(file_name)
+    n_val = int(len(all_data) * 0.1)
+    train = all_data[n_val:]
+    val = all_data[:n_val]
 
     print "Interpolating..."
     grid_x, grid_y = np.mgrid[0:MAX_X, 0:MAX_Y]
-    points = np.array([ll_to_pixel(lat, lon) for _, lat, lon in all_data])
-    values = np.array([v for v, _, _ in all_data])
+    points = np.array([ll_to_pixel(lat, lon) for _, lat, lon in train])
+    values = np.array([v for v, _, _ in train])
     grid = griddata(points, values, (grid_x, grid_y), method='linear')
 
+    # Validate
+    error = []
+    for gt, lat, lon in val:
+        x, y = ll_to_pixel(lat, lon)
+        z = grid[x, y]
+        if not np.isnan(z):
+            # Some points are out of interpolation area. Exclude those.
+            error.append((gt - grid[x, y]) ** 2)
+    rmse = np.sqrt(np.mean(error))
+    print 'RMSE (min): %.2f' % rmse
 
     # Color regions by time
-    buckets = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-    buckets.reverse()
     I = Image.new('RGBA', (MAX_X, MAX_Y))
     IM = I.load()
     for x in range(MAX_X):
@@ -90,12 +99,14 @@ def start(file_name):
             x, y = ll_to_pixel(lat, lon)
             IM[int(x), int(y)] = (0, 0, 0)
 
-    out_fname = file_name + ".phantom." + str(MAX_X)
+    out_fname = str(MAX_X)
     I.save(out_fname + ".png", "PNG")
     with open(out_fname + ".metadata.json", "w") as outf:
         outf.write(json.dumps({
             "buckets": buckets,
-            "n": len(all_data)}))
+            "colors": colors,
+            "rmse" : rmse,
+            "n": len(train)}))
 
 
 if __name__ == "__main__":
